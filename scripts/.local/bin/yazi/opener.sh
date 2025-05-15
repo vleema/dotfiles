a4c8659d0d1d68274f9703c8cedafced1311dbea
vleema <vldsampaio@pm.me>
2025-05-15 15:32:53 -0300

    yazi: open pdf/images in background

--- scripts/.local/bin/yazi/opener.sh
+++ scripts/.local/bin/yazi/opener.sh
@@ -32,85 +32,85 @@ resolved_file=$(readlink -f "$1")
 # }
 #
 launch_gui() {
-	precmd="setsid -f"
-	$precmd "$@" >/dev/null 2>&1
+  precmd="setsid -f"
+  $precmd "$@" >/dev/null 2>&1
 }
 
 launch_term() {
-	if [ "$detatch" -eq 1 ]; then
-		# This is a bit of a hack.  Shell only expects a single argument into
-		# the -c flag, so it needs to be wrapped in quotes.  However, if "$@"
-		# wher passed in with `mpv "my file.mp3"`, it would not preserve the
-		# $2 having a space and would instead split it up into 3 arguments
-		# into the new shell.  Wrapping everything argument in quotes is the
-		# only general solution I could come up with.
-		command_string=""
-		while [ $# -gt 0 ]; do
-			command_string="$command_string \"$1\""
-			shift
-		done
-		setsid -f "$TERMINAL" -e "$SHELL" -c "$command_string; exec $SHELL" >/dev/null 2>&1
-	else
-		clear
-		"$@"
-	fi
+  if [ "$detatch" -eq 1 ]; then
+    # This is a bit of a hack.  Shell only expects a single argument into
+    # the -c flag, so it needs to be wrapped in quotes.  However, if "$@"
+    # wher passed in with `mpv "my file.mp3"`, it would not preserve the
+    # $2 having a space and would instead split it up into 3 arguments
+    # into the new shell.  Wrapping everything argument in quotes is the
+    # only general solution I could come up with.
+    command_string=""
+    while [ $# -gt 0 ]; do
+      command_string="$command_string \"$1\""
+      shift
+    done
+    setsid -f "$TERMINAL" -e "$SHELL" -c "$command_string; exec $SHELL" >/dev/null 2>&1
+  else
+    clear
+    "$@"
+  fi
 }
 
 case $(file --mime-type "$resolved_file" -b) in
 application/javascript | \
-	application/json | \
-	application/pgp-encrypted | \
-	application/x-subrip | \
-	inode/x-empty | \
-	text/*)
-	case "${resolved_file##*.}" in
-	org | typ)
-		# Any "document" like file ought to be in emacs
-		launch_gui emacsclient -c "$resolved_file"
-		exit
-		;;
-	esac
+  application/json | \
+  application/pgp-encrypted | \
+  application/x-subrip | \
+  inode/x-empty | \
+  text/*)
+  case "${resolved_file##*.}" in
+  org | typ)
+    # Any "document" like file ought to be in emacs
+    launch_gui emacsclient -c "$resolved_file"
+    exit
+    ;;
+  esac
 
-	launch_term nvim "$resolved_file"
-	;;
+  launch_term nvim "$resolved_file"
+  ;;
 image/*)
-	launch_term swayimg "$resolved_file"
-	;;
+  launch_term swayimg "$resolved_file"
+  ;;
 video/*)
-	launch_term mpv -quiet "$resolved_file"
-	;;
+  launch_term mpv -quiet "$resolved_file"
+  ;;
 application/epub* | \
-	application/octet-stream | \
-	application/pdf | \
-	application/postscript | \
-	application/vnd.djvu | \
-	image/vnd.djvu)
-	launch_term zathura "$resolved_file"
-	;;
+  application/octet-stream | \
+  application/pdf | \
+  application/postscript | \
+  application/vnd.djvu | \
+  image/vnd.djvu)
+  launch_term zathura "$resolved_file" &
+  ;;
 audio/* | \
-	video/x-ms-asf)
-	[ -z "$(mediainfo "$resolved_file" | grep "Cover\s*: Yes")" ] &&
-		(launch_term mpv --audio-display=no "$resolved_file") ||
-		(launch_gui mpv "$resolved_file")
-	;;
+  video/x-ms-asf)
+  [ -z "$(mediainfo "$resolved_file" | grep "Cover\s*: Yes")" ] &&
+    (launch_term mpv --audio-display=no "$resolved_file") ||
+    (launch_gui mpv "$resolved_file")
+  ;;
 application/msword | \
-	application/octet-stream | \
-	application/vnd.ms-powerpoint | \
-	application/vnd.oasis.opendocument.database | \
-	application/vnd.oasis.opendocument.formula | \
-	application/vnd.oasis.opendocument.graphics | \
-	application/vnd.oasis.opendocument.graphics-template | \
-	application/vnd.oasis.opendocument.presentation | \
-	application/vnd.oasis.opendocument.presentation-template | \
-	application/vnd.oasis.opendocument.spreadsheet | \
-	application/vnd.oasis.opendocument.spreadsheet-template | \
-	application/vnd.oasis.opendocument.text | \
-	application/vnd.ms-excel | \
-	application/vnd.openxmlformats-officedocument.presentationml.presentation | \
-	application/vnd.openxmlformats-officedocument.spreadsheetml.sheet | \
-	application/vnd.openxmlformats-officedocument.wordprocessingml.document)
-	launch_term libreoffice "$resolved_file"
-	;;
+  application/octet-stream | \
+  application/vnd.ms-powerpoint | \
+  application/vnd.oasis.opendocument.database | \
+  application/vnd.oasis.opendocument.formula | \
+  application/vnd.oasis.opendocument.graphics | \
+  application/vnd.oasis.opendocument.graphics-template | \
+  application/vnd.oasis.opendocument.presentation | \
+  application/vnd.oasis.opendocument.presentation-template | \
+  application/vnd.oasis.opendocument.spreadsheet | \
+  application/vnd.oasis.opendocument.spreadsheet-template | \
+  application/vnd.oasis.opendocument.text | \
+  application/vnd.ms-excel | \
+  application/vnd.openxmlformats-officedocument.presentationml.presentation | \
+  application/vnd.openxmlformats-officedocument.spreadsheetml.sheet | \
+  application/vnd.openxmlformats-officedocument.wordprocessingml.document)
+  launch_term libreoffice "$resolved_file"
+  ;;
 *) ;;
 esac
 
