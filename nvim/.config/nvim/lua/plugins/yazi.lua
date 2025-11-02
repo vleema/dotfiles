ec594a36682ecd35c5b2fc38cec9c253473db567
Vinicius Sampaio <vldsampaio@pm.me>
2025-11-02 09:56:00 -0300

    nvim: re-enable netrw

--- nvim/.config/nvim/lua/plugins/yazi.lua
+++ nvim/.config/nvim/lua/plugins/yazi.lua
@@ -14,16 +14,16 @@ return {
     keymaps = { show_help = "<f1>" },
   },
   -- Optional: disable netrw if you want yazi to handle directories
-  init = function()
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function()
-        local first_arg = vim.v.argv[1]
-        if first_arg and vim.fn.isdirectory(first_arg) == 1 then
-          vim.cmd("Yazi cwd")
-        end
-      end,
-    })
-  end,
+  -- init = function()
+  --   vim.g.loaded_netrw = 1
+  --   vim.g.loaded_netrwPlugin = 1
+  --   vim.api.nvim_create_autocmd("VimEnter", {
+  --     callback = function()
+  --       local first_arg = vim.v.argv[1]
+  --       if first_arg and vim.fn.isdirectory(first_arg) == 1 then
+  --         vim.cmd("Yazi cwd")
+  --       end
+  --     end,
+  --   })
+  -- end,
 }
