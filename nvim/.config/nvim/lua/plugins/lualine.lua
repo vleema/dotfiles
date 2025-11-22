d280db2fbb8a8aff9c2d4872acfd2ab2c6cb8fdf
Vinicius Sampaio <vldsampaio@pm.me>
2025-11-21 23:33:34 -0300

    nvim: simplify lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -21,31 +21,40 @@ return {
         sections = {
           lualine_a = { "mode" },
           lualine_b = {
-            { "branch" },
+            -- { "branch" },
+            -- LazyVim.lualine.root_dir({ icon = "" }),
+            -- { "filetype", icon_only = false, separator = "", padding = { left = 1, right = 0 } },
+            { LazyVim.lualine.pretty_path({ relative = "root" }) },
           },
 
           lualine_c = {
-            LazyVim.lualine.root_dir(),
-            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            { LazyVim.lualine.pretty_path() },
             {
-              "diff",
+              "diagnostics",
               symbols = {
-                added = icons.git.added,
-                modified = icons.git.modified,
-                removed = icons.git.removed,
+                error = icons.diagnostics.Error,
+                warn = icons.diagnostics.Warn,
+                info = icons.diagnostics.Info,
+                hint = icons.diagnostics.Hint,
               },
-              source = function()
-                local gitsigns = vim.b.gitsigns_status_dict
-                if gitsigns then
-                  return {
-                    added = gitsigns.added,
-                    modified = gitsigns.changed,
-                    removed = gitsigns.removed,
-                  }
-                end
-              end,
             },
+            -- {
+            --   "diff",
+            --   symbols = {
+            --     added = icons.git.added,
+            --     modified = icons.git.modified,
+            --     removed = icons.git.removed,
+            --   },
+            --   source = function()
+            --     local gitsigns = vim.b.gitsigns_status_dict
+            --     if gitsigns then
+            --       return {
+            --         added = gitsigns.added,
+            --         modified = gitsigns.changed,
+            --         removed = gitsigns.removed,
+            --       }
+            --     end
+            --   end,
+            -- },
           },
           lualine_x = {
             Snacks.profiler.status(),
@@ -68,21 +77,12 @@ return {
             color = function() return { fg = Snacks.util.color("Debug") } end,
           },
 
-          -- stylua: ignore
-          -- {
-          --   require("lazy.status").updates,
-          --   cond = require("lazy.status").has_updates,
-          --   color = function() return { fg = Snacks.util.color("Special") } end,
-          -- },
-            {
-              "diagnostics",
-              symbols = {
-                error = icons.diagnostics.Error,
-                warn = icons.diagnostics.Warn,
-                info = icons.diagnostics.Info,
-                hint = icons.diagnostics.Hint,
-              },
-            },
+            -- stylua: ignore
+            -- {
+            --   require("lazy.status").updates,
+            --   cond = require("lazy.status").has_updates,
+            --   color = function() return { fg = Snacks.util.color("Special") } end,
+            -- },
           },
           lualine_y = {
             { "progress", separator = " ", padding = { left = 1, right = 1 } },
