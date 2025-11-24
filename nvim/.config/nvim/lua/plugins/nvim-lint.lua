66115e984f112e4663d33fec452a29836fb40023
Vinicius Sampaio <vldsampaio@pm.me>
2025-11-24 12:15:54 -0300

    nvim: manually zero text linters

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -5,10 +5,12 @@ return {
     "BufNewFile",
   },
   config = function()
+    local lint = require("lint")
+    lint.linters_by_ft["text"] = {}
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = vim.api.nvim_create_augroup("lint", { clear = true }),
       callback = function()
-        require("lint").try_lint()
+        lint.try_lint()
       end,
     })
   end,
