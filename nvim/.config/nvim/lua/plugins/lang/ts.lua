908452e1c1a7191bf0bbae68338b7bcaedeca65c
Vinicius Sampaio <vldsampaio@pm.me>
2026-01-29 11:26:33 -0300

    nvim: add typecheck plugin for typescript

--- nvim/.config/nvim/lua/plugins/lang/ts.lua
+++ nvim/.config/nvim/lua/plugins/lang/ts.lua
@@ -1,1 +1,19 @@
-return { "windwp/nvim-ts-autotag", opts = {} }
+return {
+  { "windwp/nvim-ts-autotag", opts = {} },
+  {
+    "jellydn/typecheck.nvim",
+    dependencies = { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+    ft = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact" },
+    opts = {
+      debug = true,
+      mode = "quickfix", -- "quickfix" | "trouble"
+    },
+    keys = {
+      {
+        "<leader>ck",
+        "<cmd>Typecheck<cr>",
+        desc = "Run Type Check",
+      },
+    },
+  },
+}
