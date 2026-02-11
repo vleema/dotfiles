7e5b7ec9b65220254f6eb25f00376e47d596f7de
Vinicius Sampaio <vldsampaio@pm.me>
2026-02-10 21:21:29 -0300

    nvim: add claude code

--- nvim/.config/nvim/lua/plugins/copilot-chat.lua
+++ nvim/.config/nvim/lua/plugins/copilot-chat.lua
@@ -1,6 +1,7 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
   branch = "main",
+  enabled = false,
   opts = function()
     local user = vim.env.USER or "User"
     user = user:sub(1, 1):upper() .. user:sub(2)
